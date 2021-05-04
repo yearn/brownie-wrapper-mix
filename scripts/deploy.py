f14bd9d2b9dafeb616216fcb923154fc4b457e0e
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2021-05-03 23:21:53 -0400

    Update deploy.py

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,8 +3,10 @@ from pathlib import Path
 from brownie import AffiliateToken, Contract, accounts, config, network, project, web3
 from eth_utils import is_checksum_address
 
-# For later use
 import click
+# User Input
+tokenName = "abcdef"
+tokenSymbol = "ABC"
 
 API_VERSION = config["dependencies"][0].split("@")[-1]
 Vault = project.load(
@@ -13,22 +15,22 @@ Vault = project.load(
 
 def get_address(msg: str, default: str = None) -> str:
   
-val = click.prompt(msg, default=default)
+    val = click.prompt(msg, default=default)
 
-# Keep asking user for click.prompt until it passes
-while True:
+    # Keep asking user for click.prompt until it passes
+    while True:
 
-    if is_checksum_address(val):
-        return val
-    elif addr := web3.ens.address(val):
-        click.echo(f"Found ENS '{val}' [{addr}]")
-        return addr
+        if is_checksum_address(val):
+            return val
+        elif addr := web3.ens.address(val):
+            click.echo(f"Found ENS '{val}' [{addr}]")
+            return addr
 
-    click.echo(
-        f"I'm sorry, but '{val}' is not a checksummed address or valid ENS record"
-    )
-    # NOTE: Only display default once
-    val = click.prompt(msg)
+        click.echo(
+            f"I'm sorry, but '{val}' is not a checksummed address or valid ENS record"
+        )
+        # NOTE: Only display default once
+        val = click.prompt(msg)
 
 def main():
     print(f"You are using the '{network.show_active()}' network")
@@ -36,15 +38,13 @@ def main():
     print(f"You are using: 'dev' [{dev.address}]")
     token = Contract.from_explorer(get_address("Underlying token"))
     registry = get_address("Registry address,", "v2.registry.ychad.eth")
-    tokenName = token.name()
-    tokenSymbol = token.symbol()
     
     print(
         f"""
     Affiliate Token Parameters
       api: {API_VERSION}
-      vault: {tokenName}
-      symbol: '{tokenSymbol}'
+      vault: {token.name()}
+      symbol: '{token.symbol()}'
     """
     )
     
